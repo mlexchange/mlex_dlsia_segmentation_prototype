060d0234a4a3111175f4662e675970b5680838b7
TibbersHao <ghao@lbl.gov>
2024-02-20 19:12:59 -0800

    added an additional Tiled Dataset Class just for inference

--- src/tiled_dataset.py
+++ src/tiled_dataset.py
@@ -1,27 +1,23 @@
 import torch
 from tiled.client import from_uri
 
-class TiledDataset(torch.utils.data.Dataset):
+class TrainingDataset(torch.utils.data.Dataset):
     def __init__(
             self,
             data_tiled_uri,
-            mask_uri,
-            seg_uri,
+            mask_tiled_uri,
             mask_idx,
             data_tiled_api_key=None,
-            mask_api_key=None,
-            seg_api_key=None,
+            mask_tiled_api_key=None,
             shift=0,
             transform=None):
         '''
         Args:
             data_tiled_uri:      str,    Tiled URI of the input data
-            mask_uri:            str,    Tiled URI of mask
-            seg_uri:             str,    Tiled URI of segmentation results
+            mask_tiled_uri:      str,    Tiled URI of mask
             mask_idx:            list,   List for slice indexs that maps the mask to corresponding input images  
             data_tiled_api_key:  str,    Tiled API key for input data access
-            mask_api_key:        str,    Tiled API key for mask access
-            seg_api_key:         str,    Tiled API key for segmentation access
+            mask_tiled_api_key:  str,    Tiled API key for mask access
             shift:               int,    Optional shift for pixel values in masks to bring down the class id to start from 0, (and -1 for unlabeled pixles)
             transform:           callable, if not given return PIL image
 
@@ -29,8 +25,7 @@ class TiledDataset(torch.utils.data.Dataset):
             ml_data:        tuple, (data_tensor, mask_tensor)
         '''
         self.data_client = from_uri(data_tiled_uri, api_key=data_tiled_api_key)
-        self.mask_client = from_uri(mask_uri, api_key=mask_api_key)
-        self.seg_client = from_uri(seg_uri, api_key=seg_api_key)
+        self.mask_client = from_uri(mask_tiled_uri, api_key=mask_tiled_api_key)
         self.mask_idx = list(mask_idx)
         self.shift = int(shift)
         self.transform = transform
@@ -45,3 +40,44 @@ class TiledDataset(torch.utils.data.Dataset):
             return self.transform(data), mask
         else:
             return data, mask
+        
+class InferenceDataset(torch.utils.data.Dataset):
+    def __init__(
+            self,
+            data_tiled_uri,
+            mask_tiled_uri,
+            seg_tiled_uri,
+            mask_idx,
+            data_tiled_api_key=None,
+            mask_tiled_api_key=None,
+            seg_tiled_api_key=None,
+            transform=None):
+        '''
+        Args:
+            data_tiled_uri:      str,    Tiled URI of the input data
+            mask_tiled_uri:      str,    Tiled URI of mask
+            seg_tiled_uri:       str,    Tiled URI of segmentation results
+            mask_idx:            list,   List for slice indexs that maps the mask to corresponding input images, used for slicing here  
+            data_tiled_api_key:  str,    Tiled API key for input data access
+            mask_tiled_api_key:  str,    Tiled API key for mask access
+            seg_tiled_api_key:   str,    Tiled API key for segmentation results
+            transform:           callable, if not given return PIL image
+
+        Return:
+            ml_data:             data_tensor
+        '''
+        self.data_client = from_uri(data_tiled_uri, api_key=data_tiled_api_key)
+        self.mask_client = from_uri(mask_tiled_uri, api_key=mask_tiled_api_key)
+        self.seg_client = from_uri(seg_tiled_uri, api_key=seg_tiled_api_key)
+        self.mask_idx = list(mask_idx)
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.mask_idx)
+
+    def __getitem__(self, idx):
+        data = self.data_client[self.mask_idx[idx],]
+        if self.transform:
+            return self.transform(data)
+        else:
+            return data
