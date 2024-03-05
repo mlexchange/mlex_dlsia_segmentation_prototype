d7686ed31c6b147264362e9db69fa7c8fb7c01c8
Tibbers Hao <ghao@lbl.gov>
2024-03-04 20:28:30 -0800

    added I/O validation, modified TiledDataset, changed output container hardcoding

--- src/segment.py
+++ src/segment.py
@@ -1,6 +1,6 @@
 import  argparse
 from    network             import  load_network
-from    parameters          import  MSDNetParameters, TUNetParameters, TUNet3PlusParameters
+from    parameters          import  IOParameters, MSDNetParameters, TUNetParameters, TUNet3PlusParameters
 from    qlty.qlty2D         import  NCYXQuilt
 from    seg_utils           import  custom_collate, segment
 from    tiled_dataset       import  TiledDataset
@@ -21,6 +21,10 @@ if __name__ == '__main__':
         # Load parameters
         parameters = yaml.safe_load(file)
 
+    # Validate and load I/O related parameters
+    io_parameters = parameters['io_parameters']
+    io_parameters = IOParameters(**io_parameters)
+
     # Detect which model we have, then load corresponding parameters 
     raw_parameters = parameters['model_parameters']
     network = raw_parameters['network']
@@ -36,17 +40,19 @@ if __name__ == '__main__':
     assert model_parameters, f"Received Unsupported Network: {network}"
     
     print('Parameters loaded successfully.')
-    
+
     dataset = TiledDataset(
-        data_tiled_uri=parameters['data_tiled_uri'],
-        mask_idx=parameters['mask_idx'], # Keeping this for a quick inference for now, in future this will be out with updates from app.
-        data_tiled_api_key=parameters['data_tiled_api_key'],
+        data_tiled_uri=io_parameters.data_tiled_uri,
+        data_tiled_api_key=io_parameters.data_tiled_api_key,
+        mask_tiled_uri=io_parameters.mask_tiled_uri,
+        mask_tiled_api_key=io_parameters.mask_tiled_api_key,
+        workflow_type=io_parameters.workflow_type,
         qlty_window=model_parameters.qlty_window,
         qlty_step=model_parameters.qlty_step,
         qlty_border=model_parameters.qlty_border,
         transform=transforms.ToTensor()
         )
-    
+
     # Set Dataloader parameters (Note: we randomly shuffle the training set upon each pass)
     inference_loader_params = {'batch_size': model_parameters.batch_size_inference,
                                'shuffle': model_parameters.shuffle_inference}
@@ -56,7 +62,7 @@ if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # Load Network
-    model_params_path = f"{parameters['uid']}/{parameters['uid']}_{network}.pt"
+    model_params_path = f"{io_parameters.uid}/{io_parameters.uid}_{network}.pt"
     net = load_network(network, model_params_path)
 
     qlty_object = NCYXQuilt(X=dataset.data_client.shape[-1], 
@@ -68,17 +74,11 @@ if __name__ == '__main__':
     # Start segmentation
     seg_result = segment(net, device, inference_loader, qlty_object)
     
-    # Save results back to Tiled
-    # TODO: Change the hard-coding of container keys
-    container_keys = ["mlex_store", 'rec20190524_085542_clay_testZMQ_8bit', 'results']
-    
-    
     seg_result_uri, seg_result_metadata = save_seg_to_tiled(seg_result, 
-                                                            parameters['data_tiled_uri'], 
-                                                            parameters['mask_tiled_uri'],
-                                                            parameters['seg_tiled_uri'],
-                                                            parameters['seg_tiled_api_key'], 
-                                                            container_keys, 
-                                                            parameters['uid'], 
+                                                            io_parameters.data_tiled_uri, 
+                                                            io_parameters.mask_tiled_uri,
+                                                            io_parameters.seg_tiled_uri,
+                                                            io_parameters.seg_tiled_api_key,
+                                                            io_parameters.uid, 
                                                             network
                                                             )
