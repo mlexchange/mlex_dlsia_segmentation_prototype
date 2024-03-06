9f09fcb3150532319a960118d5fd2a8db5991eae
TibbersHao <ghao@lbl.gov>
2024-03-05 16:09:21 -0800

    modified save function to use new tiled_dataset class, also save mask_idx as metadata

--- src/utils.py
+++ src/utils.py
@@ -12,8 +12,7 @@ def create_directory(path):
 
 # Tiled Saving
 def save_seg_to_tiled(seg_result,
-                      data_tiled_uri,
-                      mask_tiled_uri,
+                      tiled_dataset,
                       seg_tiled_uri,
                       seg_tiled_api_key,
                       uid,
@@ -24,11 +23,13 @@ def save_seg_to_tiled(seg_result,
     last_container = last_container.create_container(key=uid)
 
     metadata={
-        'data_tiled_uri': data_tiled_uri,
-        'mask_uri': mask_tiled_uri, 
+        'data_tiled_uri': tiled_dataset.data_tiled_uri,
+        'mask_uri': tiled_dataset.mask_tiled_uri, 
+        'mask_idx': tiled_dataset.mask_idx,
         'uid': uid,
         'model': model, 
         }
+    print(f'metadata: {metadata}')
     seg_result = last_container.write_array(key="seg_result", array=seg_result, metadata=metadata)
     print("Segmentaion result array saved in following uri: ", seg_result.uri)
     return seg_result.uri, seg_result.metadata   
