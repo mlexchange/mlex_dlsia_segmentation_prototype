6c57917a29e07a3326221caea1c44fa73827fe8b
Wiebke Köpp <wiebke.koepp@googlemail.com>
2024-03-11 19:30:58 +0000

    Result of `pre-commit run --all-files` and manual flake8 fixes
    Manual fixes:
    E501 line too long
    src/seg_utils.py:14:5: F841 local variable 'first_data' is assigned to but never used
    src/network.py:22:42: E712 comparison to False should be 'if cond is False:' or 'if not cond:'

--- src/network.py
+++ src/network.py
@@ -1,26 +1,26 @@
 import glob
 import logging
+
 import numpy as np
 import torch.nn as nn
-
-from dlsia.core.networks import msdnet, tunet, tunet3plus, smsnet
 from dlsia.core import helpers
-
+from dlsia.core.networks import msdnet, smsnet, tunet, tunet3plus
 from dlsia.core.networks.baggins import model_baggin
 
-#============================MSDNet==================================#
+
+# ============================MSDNet==================================#
 def build_msdnet(
-        in_channels,
-        out_channels,
-        msdnet_parameters,
-        activation,
-        normalization,
-        convolution,
-        final_layer = nn.Softmax(dim=1),
-        ):
-    
-    if msdnet_parameters.custom_dilation == False:
-        logging.info(f'Using maximum dilation: {msdnet_parameters.max_dilation}')
+    in_channels,
+    out_channels,
+    msdnet_parameters,
+    activation,
+    normalization,
+    convolution,
+    final_layer=nn.Softmax(dim=1),
+):
+
+    if not msdnet_parameters.custom_dilation:
+        logging.info(f"Using maximum dilation: {msdnet_parameters.max_dilation}")
         network = msdnet.MixedScaleDenseNetwork(
             in_channels=in_channels,
             out_channels=out_channels,
@@ -30,12 +30,14 @@ def build_msdnet(
             activation=activation,
             normalization=normalization,
             final_layer=final_layer,
-            convolution=convolution
-            )
+            convolution=convolution,
+        )
     else:
-        dilation_array = [int(x) for x in msdnet_parameters.dilation_array.strip('[]').split(',')]
+        dilation_array = [
+            int(x) for x in msdnet_parameters.dilation_array.strip("[]").split(",")
+        ]
         dilation_array = np.array(dilation_array)
-        logging.info(f'Using custom dilation: {dilation_array}')
+        logging.info(f"Using custom dilation: {dilation_array}")
         network = msdnet.MixedScaleDenseNetwork(
             in_channels=in_channels,
             out_channels=out_channels,
@@ -45,109 +47,113 @@ def build_msdnet(
             activation=activation,
             normalization=normalization,
             final_layer=final_layer,
-            convolution=convolution
-            )
+            convolution=convolution,
+        )
     return [network]
 
-#============================TUNet==================================#
+
+# ============================TUNet==================================#
 def build_tunet(
-        in_channels,
-        out_channels,
-        image_shape,
-        tunet_parameters,
-        activation,
-        normalization,
-        ):
+    in_channels,
+    out_channels,
+    image_shape,
+    tunet_parameters,
+    activation,
+    normalization,
+):
 
     network = tunet.TUNet(
-            image_shape=image_shape,
-            in_channels=in_channels,
-            out_channels=out_channels,
-            depth=tunet_parameters.depth,
-            base_channels=tunet_parameters.base_channels,
-            growth_rate=tunet_parameters.growth_rate,
-            hidden_rate=tunet_parameters.hidden_rate,
-            activation=activation,
-            normalization=normalization,
-            )
+        image_shape=image_shape,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        depth=tunet_parameters.depth,
+        base_channels=tunet_parameters.base_channels,
+        growth_rate=tunet_parameters.growth_rate,
+        hidden_rate=tunet_parameters.hidden_rate,
+        activation=activation,
+        normalization=normalization,
+    )
     return [network]
 
-#============================TUNet3+==================================#
+
+# ============================TUNet3+==================================#
 def build_tunet3plus(
-        in_channels,
-        out_channels,
-        image_shape,
-        tunet3plus_parameters,
-        activation,
-        normalization,
-        ):
+    in_channels,
+    out_channels,
+    image_shape,
+    tunet3plus_parameters,
+    activation,
+    normalization,
+):
 
     network = tunet3plus.TUNet3Plus(
-            image_shape=image_shape,
-            in_channels=in_channels,
-            out_channels=out_channels,
-            depth=tunet3plus_parameters.depth,
-            base_channels=tunet3plus_parameters.base_channels,
-            growth_rate=tunet3plus_parameters.growth_rate,
-            hidden_rate=tunet3plus_parameters.hidden_rate,
-            carryover_channels=tunet3plus_parameters.carryover_channels,
-            activation=activation,
-            normalization=normalization,
-            )
+        image_shape=image_shape,
+        in_channels=in_channels,
+        out_channels=out_channels,
+        depth=tunet3plus_parameters.depth,
+        base_channels=tunet3plus_parameters.base_channels,
+        growth_rate=tunet3plus_parameters.growth_rate,
+        hidden_rate=tunet3plus_parameters.hidden_rate,
+        carryover_channels=tunet3plus_parameters.carryover_channels,
+        activation=activation,
+        normalization=normalization,
+    )
     return [network]
 
-#============================SMSNet Ensemble==================================#
-def construct_2dsms_ensembler(n_networks,
-                              in_channels,
-                              out_channels,
-                           layers,
-                           alpha = 0.0,
-                           gamma = 0.0,
-                           hidden_channels = None,
-                           dilation_choices = [1,2,3,4],
-                           P_IL = 0.995,
-                           P_LO = 0.995,
-                           P_IO = True,
-                           parameter_bounds = None,
-                           max_trial=100,
-                           network_type="Regression",
-                           parameter_counts_only = False
-                           ):
+
+# ============================SMSNet Ensemble==================================#
+def construct_2dsms_ensembler(
+    n_networks,
+    in_channels,
+    out_channels,
+    layers,
+    alpha=0.0,
+    gamma=0.0,
+    hidden_channels=None,
+    dilation_choices=[1, 2, 3, 4],
+    P_IL=0.995,
+    P_LO=0.995,
+    P_IO=True,
+    parameter_bounds=None,
+    max_trial=100,
+    network_type="Regression",
+    parameter_counts_only=False,
+):
 
     networks = []
 
     layer_probabilities = {
-        'LL_alpha': alpha,
-        'LL_gamma': gamma,
-        'LL_max_degree': layers,
-        'LL_min_degree': 1,
-        'IL': P_IL,
-        'LO': P_LO,
-        'IO': P_IO,
+        "LL_alpha": alpha,
+        "LL_gamma": gamma,
+        "LL_max_degree": layers,
+        "LL_min_degree": 1,
+        "IL": P_IL,
+        "LO": P_LO,
+        "IO": P_IO,
     }
-
 
     if parameter_counts_only:
         assert parameter_bounds is None
 
     if hidden_channels is None:
-        hidden_channels = [ 3*out_channels ]
+        hidden_channels = [3 * out_channels]
 
     for _ in range(n_networks):
         ok = False
         count = 0
         while not ok:
             count += 1
-            this_net = smsnet.random_SMS_network(in_channels=in_channels,
-                                                    out_channels=out_channels,
-                                                    layers=layers,
-                                                    dilation_choices=dilation_choices,
-                                                    hidden_out_channels=hidden_channels,
-                                                    layer_probabilities=layer_probabilities,
-                                                    sizing_settings=None,
-                                                    dilation_mode="Edges",
-                                                    network_type=network_type,
-                                                    )
+            this_net = smsnet.random_SMS_network(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                layers=layers,
+                dilation_choices=dilation_choices,
+                hidden_out_channels=hidden_channels,
+                layer_probabilities=layer_probabilities,
+                sizing_settings=None,
+                dilation_mode="Edges",
+                network_type=network_type,
+            )
             pcount = helpers.count_parameters(this_net)
             if parameter_bounds is not None:
                 if pcount > min(parameter_bounds):
@@ -164,13 +170,16 @@ def construct_2dsms_ensembler(n_networks,
                     networks.append(this_net)
     return networks
 
-def build_smsnet_ensemble(
-        in_channels,
-        out_channels,
-        ensemble_parameters,
-        ):
 
-    dilation_choices = [int(x) for x in ensemble_parameters.dilation_choices.strip('[]').split(',')]
+def build_smsnet_ensemble(
+    in_channels,
+    out_channels,
+    ensemble_parameters,
+):
+
+    dilation_choices = [
+        int(x) for x in ensemble_parameters.dilation_choices.strip("[]").split(",")
+    ]
     list_of_networks = construct_2dsms_ensembler(
         n_networks=ensemble_parameters.num_networks,
         in_channels=in_channels,
@@ -182,19 +191,20 @@ def build_smsnet_ensemble(
         dilation_choices=dilation_choices,
         parameter_bounds=None,
         max_trial=ensemble_parameters.max_trial,
-        network_type='Classification',
+        network_type="Classification",
         parameter_counts_only=False,
-        )
-    print(f'Number of SMSNet constructed: {len(list_of_networks)}')
+    )
+    print(f"Number of SMSNet constructed: {len(list_of_networks)}")
     return list_of_networks
 
+
 def build_network(
-        network,
-        data_shape,
-        num_classes,
-        parameters,
-        ):
-    if len(data_shape)==3:
+    network,
+    data_shape,
+    num_classes,
+    parameters,
+):
+    if len(data_shape) == 3:
         in_channels = 1
         image_shape = data_shape[1:]
     else:
@@ -211,9 +221,9 @@ def build_network(
         normalization = getattr(nn, parameters.normalization)
 
     if parameters.convolution is not None:
-        convolution = getattr(nn, parameters.convolution)   
+        convolution = getattr(nn, parameters.convolution)
 
-    if network == 'MSDNet':
+    if network == "MSDNet":
         network = build_msdnet(
             in_channels,
             out_channels,
@@ -221,9 +231,9 @@ def build_network(
             activation,
             normalization,
             convolution,
-            )
-        
-    elif network == 'TUNet':
+        )
+
+    elif network == "TUNet":
         network = build_tunet(
             in_channels,
             out_channels,
@@ -231,9 +241,9 @@ def build_network(
             parameters,
             activation,
             normalization,
-            )
-        
-    elif network == 'TUNet3+':
+        )
+
+    elif network == "TUNet3+":
         network = build_tunet3plus(
             in_channels,
             out_channels,
@@ -241,37 +251,39 @@ def build_network(
             parameters,
             activation,
             normalization,
-            )
-        
-    elif network == 'SMSNetEnsemble':
+        )
+
+    elif network == "SMSNetEnsemble":
         network = build_smsnet_ensemble(
             in_channels,
             out_channels,
             parameters,
-            )
-        
+        )
+
     return network
 
+
 def load_network(
-        network,
-        params_path,
-        ):
-    
-    if network == 'MSDNet':
+    network,
+    params_path,
+):
+
+    if network == "MSDNet":
         network = msdnet.MSDNetwork_from_file(params_path)
-        
-    elif network == 'TUNet':
+
+    elif network == "TUNet":
         network = tunet.TUNetwork_from_file(params_path)
-        
-    elif network == 'TUNet3+':
+
+    elif network == "TUNet3+":
         network = tunet3plus.TUNetwork3Plus_from_file(params_path)
-        
+
     return network
+
 
 def baggin_smsnet_ensemble(network_dir):
     net_files = glob.glob(f"{network_dir}/*.pt")
     list_of_smsnet = []
     for network in net_files:
         list_of_smsnet.append(smsnet.SMSNetwork_from_file(network))
-    ensemble = model_baggin(models=list_of_smsnet, model_type='classification')
+    ensemble = model_baggin(models=list_of_smsnet, model_type="classification")
     return ensemble
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import os
+from urllib.parse import urlparse, urlunparse
+
+import numpy as np
 from tiled.client import from_uri
 from tiled.structures.array import ArrayStructure
-import numpy as np
-from urllib.parse import urlparse, urlunparse
+
 
 # Create directory
 def create_directory(path):
@@ -12,15 +14,24 @@ def create_directory(path):
     else:
         print(f"Local directory '{path}' already exsists.")
 
+
 def ensure_parent_containers(tiled_uri, tiled_api_key):
     parsed_url = urlparse(tiled_uri)
     path = parsed_url.path
     # Splitting path into parts
-    path_parts = path.split('/')[1:]  # Split and remove the first empty element
-    root_path = '/'.join(path_parts[:3])
-    tiled_root = urlunparse((parsed_url.scheme, parsed_url.netloc, root_path, 
-                             parsed_url.params, parsed_url.query, parsed_url.fragment))
-    
+    path_parts = path.split("/")[1:]  # Split and remove the first empty element
+    root_path = "/".join(path_parts[:3])
+    tiled_root = urlunparse(
+        (
+            parsed_url.scheme,
+            parsed_url.netloc,
+            root_path,
+            parsed_url.params,
+            parsed_url.query,
+            parsed_url.fragment,
+        )
+    )
+
     last_container = from_uri(tiled_root, api_key=tiled_api_key)
 
     for part in path_parts:
@@ -33,36 +44,42 @@ def ensure_parent_containers(tiled_uri, tiled_api_key):
 
 # Tiled Saving
 def allocate_array_space(
-                      tiled_dataset,
-                      seg_tiled_uri,
-                      seg_tiled_api_key,
-                      uid,
-                      model,
-                      array_name,
-                      ):
-    
-    
+    tiled_dataset,
+    seg_tiled_uri,
+    seg_tiled_api_key,
+    uid,
+    model,
+    array_name,
+):
+
     last_container = ensure_parent_containers(seg_tiled_uri, seg_tiled_api_key)
-    
-    assert uid not in last_container.keys(), f'uid_save: {uid} already existed in Tiled Server'
+
+    assert (
+        uid not in last_container.keys()
+    ), f"uid_save: {uid} already existed in Tiled Server"
 
     last_container = last_container.create_container(key=uid)
-    array_shape = tiled_dataset.mask_client.shape if tiled_dataset.mask_client else tiled_dataset.data_client.shape
-    structure = ArrayStructure.from_array(np.zeros(array_shape,dtype=np.int8))
+    array_shape = (
+        tiled_dataset.mask_client.shape
+        if tiled_dataset.mask_client
+        else tiled_dataset.data_client.shape
+    )
+    structure = ArrayStructure.from_array(np.zeros(array_shape, dtype=np.int8))
     # For now, only save image 1 by 1 regardless of the batch_size_inference.
     structure.chunks = ((1,) * array_shape[0], (array_shape[1],), (array_shape[2],))
 
-    metadata={
-        'data_tiled_uri': tiled_dataset.data_tiled_uri,
-        'mask_uri': tiled_dataset.mask_tiled_uri, 
-        'mask_idx': tiled_dataset.mask_idx,
-        'uid': uid,
-        'model': model, 
-        }
+    metadata = {
+        "data_tiled_uri": tiled_dataset.data_tiled_uri,
+        "mask_uri": tiled_dataset.mask_tiled_uri,
+        "mask_idx": tiled_dataset.mask_idx,
+        "uid": uid,
+        "model": model,
+    }
 
-    array_client = last_container.new(structure_family='array',
-                                      structure=structure,
-                                      key=array_name,
-                                      metadata=metadata,
-                                      )
+    array_client = last_container.new(
+        structure_family="array",
+        structure=structure,
+        key=array_name,
+        metadata=metadata,
+    )
     return array_client
