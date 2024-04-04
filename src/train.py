69442a79b36f11d2f093023e81fb43c3372bf889
xiaoyachong <150726549+xiaoyachong@users.noreply.github.com>
2024-04-04 13:30:53 -0700

    call DLSIA’s Trainer and pass dvclive obj to it

--- src/train.py
+++ src/train.py
@@ -7,8 +7,8 @@ import torch.optim as optim
 import yaml
 from torchvision import transforms
 
-from dvclive import Live
 from dlsia.core.train_scripts import Trainer
+from dvclive import Live
 from network import build_network
 from parameters import (
     IOParameters,
@@ -20,6 +20,7 @@ from parameters import (
 from seg_utils import crop_split_load, train_segmentation
 from tiled_dataset import TiledDataset
 from utils import create_directory
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
